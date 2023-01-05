2a707ed8ec07a7be916626e99609f2e3130b82bf
LFHDeveloper <111329000+LFHDeveloper@users.noreply.github.com>
2023-01-05 12:57:19 -0500

    Update webflow-mapcode.js

--- webflow-mapcode.js
+++ webflow-mapcode.js
@@ -39,7 +39,7 @@ let virginia = {lat: 37.720772, lng: -78.789190};
 
     let locations = addresses;
     
-    function init() {
+    function initMap() {
         console.log("addresses " + addresses);
       
         if(locations === null) { // if no locations, let page know
